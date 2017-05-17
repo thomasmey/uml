caf9757e3c31bb04fa57fb60304ff97686a4a2d0
Thomas Meyer <thomas@m3y3r.de>
2017-05-17 22:57:16 +0200

    Pimp Fedora 26 install script

--- fedora-26-install.sh
+++ fedora-26-install.sh
@@ -1,3 +1,17 @@
 #!/bin/sh
-#../linux/linux mem=1280m ubd0=/home/thomas/source/uml-images/Fedora.img umid=fedora22 eth0=tuntap,,ee:12:b7:84:00:e0,192.168.4.150 root=/dev/ubda3 plymouth.enable=0 con=pts
-../linux/linux mem=1280m ubd0=Fedora-26.img ubd1=/home/thomas/Downloads/Fedora-Server-netinst-x86_64-26-Alpha-1.7.iso umid=fedora26 initrd=initrd.img inst.stage2=hd:LABEL=Fedora-S-dvd-x86_64-26 quiet
+LINUX_DIR=/home/thomas/source/linux-um
+ISO_FILE=Fedora-Workstation-Live-x86_64-26_Alpha-1.7.iso
+INITRD=fedora26-initrd.cpio.gz
+
+if [ ! -f "$ISO_FILE" ]; then
+   curl -OL "https://download.fedoraproject.org/pub/fedora/linux/releases/test/26_Alpha/Workstation/x86_64/iso/$ISO_FILE" 
+fi
+
+cp config $LINUX_DIR/.config
+make ARCH=um -C $LINUX_DIR/ -j$(nproc)
+
+if [ ! -f "$INITRD" ]; then
+   iso-read -i $ISO_FILE -e isolinux/initrd.img --output-file $INITRD
+fi
+
+$LINUX_DIR/linux mem=1280m ubd0=Fedora-26.img ubd1=$ISO_FILE umid=fedora26 initrd=fedora26-initrd.cpio.gz inst.stage2=hd:LABEL=Fedora-S-dvd-x86_64-26 quiet
