4bfc3ad4bafa54fcd8a83769196eef26b86f1472
Thomas Meyer <thomas@m3y3r.de>
2017-05-17 23:34:33 +0200

    Some fixes for Fedora 26 install script

--- fedora-26-install.sh
+++ fedora-26-install.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 LINUX_DIR=/home/thomas/source/linux-um
 ISO_FILE=Fedora-Workstation-Live-x86_64-26_Alpha-1.7.iso
-INITRD=fedora26-initrd.cpio.gz
+INITRD=fedora26-initrd.cpio.xz
+DISK=Fedora26.img
 
 if [ ! -f "$ISO_FILE" ]; then
    curl -OL "https://download.fedoraproject.org/pub/fedora/linux/releases/test/26_Alpha/Workstation/x86_64/iso/$ISO_FILE" 
@@ -14,4 +15,8 @@ if [ ! -f "$INITRD" ]; then
    iso-read -i $ISO_FILE -e isolinux/initrd.img --output-file $INITRD
 fi
 
-$LINUX_DIR/linux mem=1280m ubd0=Fedora-26.img ubd1=$ISO_FILE umid=fedora26 initrd=fedora26-initrd.cpio.gz inst.stage2=hd:LABEL=Fedora-S-dvd-x86_64-26 quiet
+if [ ! -f "$DISK" ]; then
+   truncate -s 2G $DISK
+fi
+
+$LINUX_DIR/linux mem=1280m ubd0=$DISK ubd1=$ISO_FILE umid=fedora26 initrd=$INITRD inst.stage2=hd:LABEL=Fedora-S-dvd-x86_64-26 
