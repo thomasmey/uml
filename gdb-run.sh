4e374f6b9cbba007f527aba159aef2c53ce89fcd
Thomas Meyer <thomas@m3y3r.de>
2017-05-22 14:02:00 +0200

    Add gdb run helper

---
+++ gdb-run.sh
@@ -0,0 +1,2 @@
+ ../linux/linux mem=1280m ubd0=fedora26-i386.img ubd1=Fedora-Server-netinst-x86_64-26-Alpha-1.7.iso umid=fedora26 initrd=fedora26-initrd.cpio.xz inst.stage2=hd:LABEL=Fedora-S-dvd-x86_64-26 plymouth.enable=0 eth0=tuntap,,,192.168.5.1 ip=192.168.5.2::192.168.5.1:255.255.255.0:fry:eth0:none nameserver=192.168.0.1
+
